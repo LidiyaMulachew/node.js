0c87c8d9b596765a7a02932425f6c1fea2ccf9fc
Yonatan Abera <yonatanabera2007@gmail.com>
2023-08-31 17:47:16 +0300

    Product all and show are working, and was trying to fix patch method

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const database = (module.exports = () => {
   }
 });
 
-//database();
+database();
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
   }
   next();
 });
+
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 app.use((req, res, next) => {
